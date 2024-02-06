7a3fa46187ad01476c4c43b730a670189b740ce6
AlessandraGoncalves37 <134939121+AlessandraGoncalves37@users.noreply.github.com>
2024-02-06 10:40:31 -0300

    Add files via upload

---
+++ Tela de Login/cadastrar.php
@@ -0,0 +1,25 @@
+<!DOCTYPE html>
+<html lang="pt-br">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Projeto Login</title>
+    <link rel="stylesheet" href="estilo.css">
+
+</head>
+<body>
+<div id="corpo-form-cad">
+    <h1>Cadastrar</h1>
+    <form method="post" action="processa.php">
+        <input type="text" placeholder="Nome Completo">
+        <input type="text" placeholder="Telefone">
+        <input type="email" placeholder="Usuario">
+        <input type="passaword" placeholder="Senha">
+        <input type="passaword" placeholder="Confirmar Senha">    
+        <input type="submit" value="Cadastrar">
+        <!-- hff -->
+    </form>
+</div>
+</body>
+</html>
---
+++ Tela de Login/index.php
@@ -0,0 +1,22 @@
+<!DOCTYPE html>
+<html lang="pt-br">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Projeto Login</title>
+    <link rel="stylesheet" href="estilo.css">
+
+</head>
+<body>
+<div id="corpo-form">
+    <h1>Entrar</h1>
+    <form method="post" action="processa.php">
+        <input type="email" placeholder="Usuario">
+        <input type="passaword" placeholder="Senha">    
+        <input type="submit" value="Acessar">
+        <a href="cadastrar.php">Ainda não é inscrito? <strong>Cadastre-se!</strong></a>
+    </form>
+</div>
+</body>
+</html>
